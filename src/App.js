474661b083cdf325b49b409d53e9653ca3dd0d5d
Arjun Khanduri <arjun99.khanduri@gmail.com>
2021-07-24 23:52:30 +0530

    Dynamic login created

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import './App.css';
 import ChatFeed from './components/ChatFeed';
 import Login from './components/Login';
 
-const App = ()  => {
-    if(!localStorage.getItem('username')){
+const App = () => {
+    if (!localStorage.getItem('username')) {
         return <Login />
     }
-    return(
+    const username = localStorage.getItem('username');
+    const password = localStorage.getItem('password');
+    return (
         <ChatEngine
             height="100vh"
             projectID="
             580b4cb8-4e2e-4b54-a583-a15a3baaff19"
-            userName="Arjun"
-            userSecret="Arjun"
-            renderChatFeed = {(chatAddProps) => <ChatFeed {...chatAddProps} />}
+            userName={username}
+            userSecret={password}
+            renderChatFeed={(chatAddProps) => <ChatFeed {...chatAddProps} />}
         />
     );
 }
