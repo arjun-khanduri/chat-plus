4f5d22ffb3ae1a48a583ba0dc44494d1dee96aff
Arjun Khanduri <arjun99.khanduri@gmail.com>
2021-07-24 20:57:02 +0530

    Created message textbox

--- src/components/MessageForm.js
+++ src/components/MessageForm.js
@@ -1,7 +1,23 @@
+import { useState } from 'react';
+
+
 const MessageForm = () => {
+    const [value, setValue] = useState('');
+    const handleSubmit = () => {
+
+    }
+
+    const handleChange = () => {
+
+    }
     return (
-        <div>
-        </div>
+        <form className="message-form" onSubmit={handleSubmit}>
+            <input className="message-input"
+                placeholder="Type a message..."
+                value={value}
+                onChange={handleChange}
+                onSubmit={handleSubmit} />
+        </form>
     );
 }
 
