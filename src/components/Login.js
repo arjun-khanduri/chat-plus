2ea8706d31deec1a1d3fe8c18e7c43dec2a89bd7
Arjun Khanduri <arjun99.khanduri@gmail.com>
2021-07-25 00:36:10 +0530

    Minor UI Changes

--- src/components/Login.js
+++ src/components/Login.js
@@ -23,13 +23,13 @@ const Login = () => {
     return (
         <div className="wrapper">
             <div className="form">
-                <h1 className="title">Chat Plus</h1>
+                <h1 className="title" style={{ borderBottom: "2px solid white", paddingBottom: "25px" }}>Chat Plus</h1>
                 <form onSubmit={handleSubmit}>
                     <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} className="input" placeholder="Username" required />
                     <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="input" placeholder="Password" required />
                     <div align="center">
                         <button type="submit" className="button">
-                            <span>Start Chatting</span>
+                            <span>Sign In</span>
                         </button>
                     </div>
                     <h2 className="error">{error}</h2>
